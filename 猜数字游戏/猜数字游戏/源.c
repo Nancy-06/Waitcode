c51c0b52320e80a3c87944cc2cc5fb278c2e4d6e
Nancy-06 <1456387238@qq.com>
2019-05-11 11:07:29 +0800

    提交代码

---
+++ 猜数字游戏/猜数字游戏/源.c
@@ -0,0 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+void menu()
+{
+	printf("****************************************\n");
+	printf("*******************1.play****************\n");
+	printf("*******************0.exit****************\n");
+	printf("****************************************\n");
+}
+void game()
+{
+	int random = rand() % 100 + 1;
+	int input = 0;
+	while (1)
+	{
+		printf("请输入猜的数字：");
+			scanf("%d", &input);
+		if (input > random)
+		{
+			printf("猜大了\n");
+		}
+		else if (input <random)
+		{
+			printf("猜小了\n");
+		}
+		else
+		{
+			printf("恭喜你，猜对了\n");
+		}
+	}
+}
+int main()
+{
+	int input = 0;
+	srand((unsigned)time(NULL));
+	do
+	{
+		menu();
+		printf("请输入：");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			game();
+			break;
+		case 0:
+			printf("退出游戏\n");
+			break;
+		default:
+			printf("输入错误，请重新输入\n");
+			break;
+		}
+	} 
+	while (input);
+	system("pause");
+	return 0;
+}
